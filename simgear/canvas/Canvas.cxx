a6bd61315985f6c64c3b397030ff30283caecacd
James Turner <zakalawe@mac.com>
2022-03-09 09:57:05 +0000

    Remove log message, now property changes are stable

--- simgear/canvas/Canvas.cxx
+++ simgear/canvas/Canvas.cxx
@@ -137,9 +137,6 @@ namespace canvas
     _status(node, "status"),
     _status_msg(node, "status-msg")
   {
-    SG_LOG(SG_GENERAL, SG_ALERT, "Canvas constructor:"
-            " node=" << node->getPath(true /*simplify*/)
-            );
     // Looks like we need to propogate value changes upwards.
     node->setAttribute(SGPropertyNode::VALUE_CHANGED_DOWN, true);
     _status = 0;
