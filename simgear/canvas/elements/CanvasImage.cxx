ranguli/simgear

commit 27e8d3a3087bd6e53561e1ed9a74e7eb9d7a6639

Canvas: Use TRIANGLES instead of GL_QUADS
GL_QUADS is deprecated in the OpenGL core profile.

simgear/canvas/elements/CanvasImage.cxx
@@ -143,8 +143,8 @@ namespace canvas
     _colors->setDataVariance(osg::Object::DYNAMIC);
     _geom->setColorArray(_colors, osg::Array::BIND_OVERALL);
 
-    _prim = new osg::DrawArrays(osg::PrimitiveSet::QUADS);
-    _prim->set(osg::PrimitiveSet::QUADS, 0, 4);
+    _prim = new osg::DrawArrays(osg::PrimitiveSet::TRIANGLES);
+    _prim->set(osg::PrimitiveSet::TRIANGLES, 0, 6);
     _prim->setDataVariance(osg::Object::DYNAMIC);
     _geom->addPrimitiveSet(_prim);
 
@@ -377,7 +377,7 @@ namespace canvas
 
     if( _attributes_dirty & DEST_SIZE )
     {
-      size_t num_vertices = (_slice.isValid() ? (fill ? 9 : 8) : 1) * 4;
+      size_t num_vertices = (_slice.isValid() ? (fill ? 9 : 8) : 1) * 6;
 
       if( num_vertices != _prim->getNumPrimitives() )
       {
@@ -756,21 +756,25 @@ namespace canvas
   //----------------------------------------------------------------------------
   void Image::setQuad(size_t index, const SGVec2f& tl, const SGVec2f& br)
   {
-    int i = index * 4;
-    (*_vertices)[i + 0].set(tl.x(), tl.y(), 0);
-    (*_vertices)[i + 1].set(br.x(), tl.y(), 0);
-    (*_vertices)[i + 2].set(br.x(), br.y(), 0);
-    (*_vertices)[i + 3].set(tl.x(), br.y(), 0);
+    int i = index * 6;
+    (*_vertices)[i + 0].set(tl.x(), br.y(), 0.0f);
+    (*_vertices)[i + 1].set(tl.x(), tl.y(), 0.0f);
+    (*_vertices)[i + 2].set(br.x(), tl.y(), 0.0f);
+    (*_vertices)[i + 3].set(tl.x(), br.y(), 0.0f);
+    (*_vertices)[i + 4].set(br.x(), tl.y(), 0.0f);
+    (*_vertices)[i + 5].set(br.x(), br.y(), 0.0f);
   }
 
   //----------------------------------------------------------------------------
   void Image::setQuadUV(size_t index, const SGVec2f& tl, const SGVec2f& br)
   {
-    int i = index * 4;
-    (*_texCoords)[i + 0].set(tl.x(), tl.y());
-    (*_texCoords)[i + 1].set(br.x(), tl.y());
-    (*_texCoords)[i + 2].set(br.x(), br.y());
+    int i = index * 6;
+    (*_texCoords)[i + 0].set(tl.x(), br.y());
+    (*_texCoords)[i + 1].set(tl.x(), tl.y());
+    (*_texCoords)[i + 2].set(br.x(), tl.y());
     (*_texCoords)[i + 3].set(tl.x(), br.y());
+    (*_texCoords)[i + 4].set(br.x(), tl.y());
+    (*_texCoords)[i + 5].set(br.x(), br.y());
   }
 
   //----------------------------------------------------------------------------

commit e10829f8a341244832e2d4175efd50af4ae27ac4

CanvasImage: Fix buffer overflow in vert/texcoords
Since commit 27e8d3a3087b ("Canvas: Use TRIANGLES instead of GL_QUADS"),
CanvasImage _vertices and _texCoords are assumed to have 6 elements (2
triangles instead of 1 quad), however they are still initialised with
only 4 elements, resulting in buffer overflows. Initialise with 6
elements instead.

This was caught with the help of valgrind.

Fixes: 27e8d3a3087b ("Canvas: Use TRIANGLES instead of GL_QUADS")

simgear/canvas/elements/CanvasImage.cxx
@@ -132,11 +132,11 @@ namespace canvas
     stateSet->setDataVariance(osg::Object::STATIC);
 
     // allocate arrays for the image
-    _vertices = new osg::Vec3Array(4);
+    _vertices = new osg::Vec3Array(6);
     _vertices->setDataVariance(osg::Object::DYNAMIC);
     _geom->setVertexArray(_vertices);
 
-    _texCoords = new osg::Vec2Array(4);
+    _texCoords = new osg::Vec2Array(6);
     _texCoords->setDataVariance(osg::Object::DYNAMIC);
     _geom->setTexCoordArray(0, _texCoords, osg::Array::BIND_PER_VERTEX);
 