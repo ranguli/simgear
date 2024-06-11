44ff25397f5b33105d055672ad6133863608fa48
Fernando García Liñán <fernandogarcialinan@gmail.com>
2024-06-11 19:30:03 +0200

    OSGUtils: Move definition out of header file

--- simgear/structure/OSGUtils.hxx
+++ simgear/structure/OSGUtils.hxx
@@ -1,29 +1,17 @@
-// OSGUtils.hxx - Useful templates for interfacing to Open Scene Graph
-//
-// Copyright (C) 2008  Tim Moore timoore@redhat.com
-//
-// This library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Library General Public
-// License as published by the Free Software Foundation; either
-// version 2 of the License, or (at your option) any later version.
-//
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Library General Public License for more details.
-//
-// You should have received a copy of the GNU Library General Public
-// License along with this library; if not, write to the
-// Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
-// Boston, MA  02110-1301, USA.
+/*
+ * SPDX-FileName: OSGUtils.hxx
+ * SPDX-FileComment: Useful templates for interfacing to Open Scene Graph
+ * SPDX-FileCopyrightText: Copyright (C) 2008  Tim Moore <timoore@redhat.com>
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
 
-#ifndef SIMGEAR_OSGUTILS_HXX
-#define SIMGEAR_OSGUTILS_HXX 1
+#pragma once
 
 #include <osg/CopyOp>
 #include <osg/StateAttribute>
 #include <osg/StateSet>
 #include <osgText/Text>
+
 /** Typesafe wrapper around OSG's object clone function. Something
  * very similar is in current OSG sources.
  */
@@ -295,45 +283,8 @@ struct AttributeType<Viewport>
     : public TypeHolder<StateAttribute::VIEWPORT>
 {};
 
-static osgText::Text::AlignmentType mapAlignment(const std::string& val)
-{
-    if (val == "left-top" || val == "LeftTop" || val == "LEFT_TOP")
-        return osgText::Text::LEFT_TOP;
-    else if (val == "left-center" || val == "LeftCenter" || val == "LEFT_CENTER")
-        return osgText::Text::LEFT_CENTER;
-    else if (val == "left-bottom" || val == "LeftBottom" || val == "LEFT_BOTTOM")
-        return osgText::Text::LEFT_BOTTOM;
-    else if (val == "center-top" || val == "CenterTop" || val == "CENTER_TOP")
-        return osgText::Text::CENTER_TOP;
-    else if (val == "center-center" || val == "CenterCenter" || val == "CENTER_CENTER")
-        return osgText::Text::CENTER_CENTER;
-    else if (val == "center-bottom" || val == "CenterBottom" || val == "CENTER_BOTTOM")
-        return osgText::Text::CENTER_BOTTOM;
-    else if (val == "right-top" || val == "RightTop" || val == "RIGHT_TOP")
-        return osgText::Text::RIGHT_TOP;
-    else if (val == "right-center" || val == "RightCenter" || val == "RIGHT_CENTER")
-        return osgText::Text::RIGHT_CENTER;
-    else if (val == "right-bottom" || val == "RightBottom" || val == "RIGHT_BOTTOM")
-        return osgText::Text::RIGHT_BOTTOM;
-    else if (val == "left-base-line" || val == "LeftBaseLine" || val == "LEFT_BASE_LINE")
-        return osgText::Text::LEFT_BASE_LINE;
-    else if (val == "center-base-line" || val == "CenterBaseLine" || val == "CENTER_BASE_LINE")
-        return osgText::Text::CENTER_BASE_LINE;
-    else if (val == "right-base-line" || val == "RightBaseLine" || val == "RIGHT_BASE_LINE")
-        return osgText::Text::RIGHT_BASE_LINE;
-    else if (val == "left-bottom-base-line" || val == "LeftBottomBaseLine" || val == "LEFT_BOTTOM_BASE_LINE")
-        return osgText::Text::LEFT_BOTTOM_BASE_LINE;
-    else if (val == "center-bottom-base-line" || val == "CenterBottomBaseLine" || val == "CENTER_BOTTOM_BASE_LINE")
-        return osgText::Text::CENTER_BOTTOM_BASE_LINE;
-    else if (val == "right-bottom-base-line" || val == "RightBottomBaseLine" || val == "RIGHT_BOTTOM_BASE_LINE")
-        return osgText::Text::RIGHT_BOTTOM_BASE_LINE;
-    else if (val == "base-line")
-        return osgText::Text::BASE_LINE;
-    else {
-        SG_LOG(SG_GENERAL, SG_ALERT, "ignoring unknown text-alignment '" << val << "'.");
-        return osgText::Text::BASE_LINE;
-    }
-}
+osgText::Text::AlignmentType mapAlignment(const std::string& val);
+
 } // namespace osgutils
 
 template<typename AT>
@@ -363,5 +314,3 @@ inline const AT* getStateAttribute(unsigned int unit, const osg::StateSet* ss)
                                                           ::type));
 }
 } // namespace simgear
-
-#endif
