76db75b0b03da930aa6b3b2b2f36e049b5635f01
James Turner <james@flightgear.org>
2024-10-22 14:04:32 +0100

    Raise CMake min version to 3.20
    - modernize simgear_components accordingly

--- CMakeModules/SimGearComponent.cmake
+++ CMakeModules/SimGearComponent.cmake
@@ -1,34 +1,12 @@
 
-macro(simgear_component_common name includePath sourcesList sources headers)
-    set(fc${sourcesList} ${name})
-    set(fh${sourcesList} ${name})
-    foreach(s ${sources})
-        set_property(GLOBAL
-            APPEND PROPERTY ${sourcesList} "${CMAKE_CURRENT_SOURCE_DIR}/${s}")
-        set(fc${sourcesList} "${fc${sourcesList}}#${CMAKE_CURRENT_SOURCE_DIR}/${s}")
-    endforeach()
-
-	foreach(h ${headers})
-		set_property(GLOBAL
-			APPEND PROPERTY PUBLIC_HEADERS "${CMAKE_CURRENT_SOURCE_DIR}/${h}")
-        set(fh${sourcesList} "${fh${sourcesList}}#${CMAKE_CURRENT_SOURCE_DIR}/${h}")
-
-        # also append headers to the sources list, so that IDEs find the files
-        # correctly (otherwise they are not in the project)
-        set_property(GLOBAL
-            APPEND PROPERTY ${sourcesList} "${CMAKE_CURRENT_SOURCE_DIR}/${h}")
-	endforeach()
-
-    set_property(GLOBAL APPEND PROPERTY FG_GROUPS_${sourcesList}_C "${fc${sourcesList}}@")
-    set_property(GLOBAL APPEND PROPERTY FG_GROUPS_${sourcesList}_H "${fh${sourcesList}}@")
-    
-    install (FILES ${headers}  DESTINATION include/simgear/${includePath})
-endmacro()
-
 function(simgear_component name includePath sources headers)
-    simgear_component_common(${name} ${includePath} CORE_SOURCES "${sources}" "${headers}")
+    target_sources(SimGearCore PRIVATE ${sources} ${headers})
+    install (FILES ${headers} DESTINATION include/simgear/${includePath})
 endfunction()
 
 function(simgear_scene_component name includePath sources headers)
-    simgear_component_common(${name} ${includePath} SCENE_SOURCES "${sources}" "${headers}")
+    if (TARGET SimGearScene)
+        target_sources(SimGearScene PRIVATE ${sources} ${headers})
+        install (FILES ${headers} DESTINATION include/simgear/${includePath})
+    endif()
 endfunction()
